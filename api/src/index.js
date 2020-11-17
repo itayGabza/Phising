f31d796597f1ddf0314a8d9d756fc420d17ec1b9
Itay <itay.gabza@gmail.com>
2020-11-17 09:29:28 +0200

    postman

--- api/src/index.js
+++ api/src/index.js
@@ -5,8 +5,8 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
-const { clientOrigins, serverPort } = require("./config/env.dev");
 
+const { clientOrigins, serverPort } = require("./config/env.dev");
 const { messagesRouter } = require("./messages/messages.router");
 
 /**
@@ -16,6 +16,15 @@ const { messagesRouter } = require("./messages/messages.router");
 const app = express();
 const apiRouter = express.Router();
 
+app.get('/ping', function (req, res) {
+  res.send('pong')
+});
+
+app.post('/image/upvote', function(req, res){
+  var imageId = req.body.imageId;
+  console.log(imageId);
+});
+
 /**
  *  App Configuration
  */
@@ -23,6 +32,7 @@ const apiRouter = express.Router();
 app.use(helmet());
 app.use(cors({ origin: clientOrigins }));
 app.use(express.json());
+app.use(express.urlencoded());
 
 app.use("/api", apiRouter);
 
@@ -32,6 +42,7 @@ app.use(function (err, req, res, next) {
   console.log(err);
   res.status(500).send(err.message);
 });
+
 
 /**
  * Server Activation
