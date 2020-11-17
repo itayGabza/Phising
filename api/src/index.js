9e2b857351b358b0db566184ab26c0dcf424d3ef
Asaf Polakovski <apzodon@gmail.com>
2020-11-17 09:42:48 +0200

    simple route

--- api/src/index.js
+++ api/src/index.js
@@ -5,6 +5,7 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const bodyParser = require('body-parser');
 
 const { clientOrigins, serverPort } = require("./config/env.dev");
 const { messagesRouter } = require("./messages/messages.router");
@@ -20,10 +21,6 @@ app.get('/ping', function (req, res) {
   res.send('pong')
 });
 
-app.post('/image/upvote', function(req, res){
-  var imageId = req.body.imageId;
-  console.log(imageId);
-});
 
 /**
  *  App Configuration
@@ -33,10 +30,17 @@ app.use(helmet());
 app.use(cors({ origin: clientOrigins }));
 app.use(express.json());
 app.use(express.urlencoded());
-
+app.use(bodyParser.json());
 app.use("/api", apiRouter);
 
 apiRouter.use("/messages", messagesRouter);
+
+app.post('/image/upvote', function(req, res){
+  console.log(req.body);
+  const imageId = req.body.imageId;
+  console.log(imageId);
+  res.status(200).send("success")
+});
 
 app.use(function (err, req, res, next) {
   console.log(err);
